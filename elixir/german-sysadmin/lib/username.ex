bee843dd9985942a5d397776bd3db9eb36f1041d
bmakendi <bryan.makendi@gmail.com>
2026-01-05 20:59:05 +0100

    use patterns directly

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -3,12 +3,12 @@ defmodule Username do
 
   def sanitize([character | rest]) do
     case character do
-      char when char == ?_ -> [char | sanitize(rest)]
+      ?_ -> [?_ | sanitize(rest)]
+      ?ä -> [?a | [?e | sanitize(rest)]]
+      ?ö -> [?o | [?e | sanitize(rest)]]
+      ?ü -> [?u | [?e | sanitize(rest)]]
+      ?ß -> [?s | [?s | sanitize(rest)]]
       char when char >= ?a and char <= ?z -> [char | sanitize(rest)]
-      char when char == ?ä -> [?a | [?e | sanitize(rest)]]
-      char when char == ?ö -> [?o | [?e | sanitize(rest)]]
-      char when char == ?ü -> [?u | [?e | sanitize(rest)]]
-      char when char == ?ß -> [?s | [?s | sanitize(rest)]]
       _ -> sanitize(rest)
     end
   end
